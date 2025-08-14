80c8787d2f49acf3ffc5e704cb72705bc48428e2
frankie <frankpegar55@gmail.com>
2025-08-14 21:47:27 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector('.weatherForm');
 const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
-const apiKey = " "; //put your api key here
+const apiKey = "https://frankpegar.github.io/weather-app-js/"; //put your api key here
 
 weatherForm.addEventListener("submit", async event => {
   event.preventDefault();
